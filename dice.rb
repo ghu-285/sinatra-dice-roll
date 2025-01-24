9b6a21d374f8460116961c611c381e67b90f4181
ghu-285 <151415214+ghu-285@users.noreply.github.com>
2025-01-24 20:44:29 +0000

    beanie weenie

--- dice.rb
+++ dice.rb
@@ -9,18 +9,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  '<a href="/dice/2/6"><h2>Roll two 6-sided die</h2></a>
-  <a href="/dice/2/10"><h2>Roll two 10-sided die</h2></a>
-  <a href="/dice/1/20"><h2>Roll one 20-sided die</h2></a>
-  <a href="/dice/5/4"><h2>Roll five 4-sided die</h2></a>'
-end
-
-get("/zebra") do
-  "We must add a route for each path we want to support"
-end
-
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾"
+  erb(:elephant)
 end
 
 get("/dice/2/6") do
@@ -28,10 +17,8 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  erb(:two_6)
 end
 
 get("/dice/2/10") do
@@ -39,19 +26,15 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-
-  "<h1>2d10</h1>
-  <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  erb(:two_10)
 end
 
 get("/dice/1/20") do
   die = rand(1..20)
 
-  outcome = "You rolled a #{die}."
-
-  "<h1>1d20</h1>
-  <p>#{outcome}</p>"
+  @outcome = "You rolled a #{die}."
+  erb(:one_20)
 end
 
 get("/dice/5/4") do
@@ -62,8 +45,6 @@ get("/dice/5/4") do
   die_5 = rand(1..4)
   sum = first_die + second_die + die_3 + die_4 + die_5
 
-  outcome = "You rolled a #{first_die} and a #{second_die} and a #{die_3} and a #{die_4} and a #{die_5} for a total of #{sum}."
-
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+  @outcome = "You rolled a #{first_die} and a #{second_die} and a #{die_3} and a #{die_4} and a #{die_5} for a total of #{sum}."
+  erb(:five_4)
 end
